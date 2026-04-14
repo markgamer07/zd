48dd2e31ca7f750790b08265cb6b0395fd9d41bf
markgamer07 <markgamermejster@gmail.com>
2026-04-14 14:58:45 +0600

    update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,50 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+		return 'главная страница сайта';
+	});
+Route::get('/posts', function () {
+		return 'список постов';
+	});
+Route::get('/test', function () {
+    return 'test message';
 });
+
+Route::get('/dir/test', function () {
+    return 'dir test message';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'User: ' . $name;
+});
+Route::get('/user/{surname}/{name}', function ($surname, $name) {
+    return $surname . ' ' . $name;
+});
+
+Route::get('/city/{city?}', function ($city = 'omsk') {
+    return 'City: ' . $city;
+});
+
+// 6 + 13 (сразу с исправлением конфликта)
+Route::get('/user/all', function () {
+    return 'all';
+});
+Route::get('/posts/{date}', function ($date) {
+    return $date;
+})->where('date', '\d{4}-\d{2}-\d{2}');
+
+Route::get('/users/{order}', function ($order) {
+    return $order;
+})->where('order', 'name|surname|age');
+
+Route::get('/city/{name}', function ($name) {
+    return $name;
+})->where('name');
+
+Route::get('/user/slug/{slug}', function ($slug) {
+    return 'slug';
+})->where('slug', '[a-z0-9_-]+');
+Route::get('/user/profile', function () {
+    return 'profile';
+})->name('user.profile');
+
